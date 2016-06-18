4456cb5eb5edf076ef0d31e2351262b68de27dfe
Vishal <vishaldhayagude07@gmail.com>
2016-06-18 14:28:43 +0530

    Allow Rename

--- andesit_karang_anyar/andesit_karang_anyar/doctype/driver/driver.js
+++ andesit_karang_anyar/andesit_karang_anyar/doctype/driver/driver.js
@@ -2,14 +2,5 @@
 // For license information, please see license.txt
 
 
-// additional validation on license number
-frappe.ui.form.on("Driver", "validate", function(frm) {
-    if (frm.doc.wb_driver_licence && frm.doc.wb_driver_licence.length < 12) {
-        msgprint("Licence number is too short. It should be 12 digit format.");
-       validated = false;
-    }
-     
-});
-
 
 
