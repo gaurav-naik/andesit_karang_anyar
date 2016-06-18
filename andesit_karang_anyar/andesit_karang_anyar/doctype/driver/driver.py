44d6be661b7d77ee0cffd0fd121a2c796fa75c3c
Vishal <vishaldhayagude07@gmail.com>
2016-06-18 13:08:35 +0530

    Driver: Validation Customer or Supplier mandatory

--- andesit_karang_anyar/andesit_karang_anyar/doctype/driver/driver.py
+++ andesit_karang_anyar/andesit_karang_anyar/doctype/driver/driver.py
@@ -12,12 +12,15 @@ from frappe.model.document import Document
 class Driver(Document):
 	def validate(self):
 		self.validate_licence_number()
+		self.validate_driver_cu_su()
 
 	def validate_licence_number(self):
 		if len(self.wb_driver_licence) < 12:
-			frappe.throw(_("Licnece Number is not too short. It should be 12 digit format."))
+			frappe.throw(_("License Number is not too short. It should be 12 digit format."))
 
-
+	def validate_driver_cu_su(self):
+		if not self.wb_customer and self.wb_supplier:
+			frappe.throw(_("Either Customer or Supplier must be selected."))
 
 
 	
