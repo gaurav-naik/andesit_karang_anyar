0c6de885fb2849da5f1c7152877f5d7594f55edd
Gaurav Naik <gvrvnk@gmail.com>
2016-06-22 12:42:40 +0530

    Vehicle: Driver List HTML field added.

--- andesit_karang_anyar/andesit_karang_anyar/doctype/vehicle/vehicle.js
+++ andesit_karang_anyar/andesit_karang_anyar/doctype/vehicle/vehicle.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2016, MN Technique and contributors
 // For license information, please see license.txt
+frappe.provide("erpnext.utils");
 
 frappe.ui.form.on('Vehicle', {
 	refresh: function(frm) {
-
+		$(frm.fields_dict['drivers'].wrapper)
+			.html(frappe.render_template("driver_list", cur_frm.doc.__onload))
+			.find(".btn-driver").on("click", function() {
+				new_doc("Driver");
+			});
 	}
 });
