1f4c13ebf24261cb412a04551a9f634fdcd837a6
Vishal <vishaldhayagude07@gmail.com>
2016-06-25 15:33:32 +0530

    net_weight calculated and added Incoming and outgoing load. Issue #23

--- andesit_karang_anyar/andesit_karang_anyar/doctype/weighbridge_ticket/weighbridge_ticket.js
+++ andesit_karang_anyar/andesit_karang_anyar/doctype/weighbridge_ticket/weighbridge_ticket.js
@@ -4,5 +4,13 @@
 frappe.ui.form.on('Weighbridge Ticket', {
 	refresh: function(frm) {
 
+	},
+	party_type: function(frm){
+		if (frm.doc.party_type==="Customer"){
+			frm.set_value("wbt_load_direction","Outgoing");
+		} 
+		else if (frm.doc.party_type==="Supplier") {
+			frm.set_value("wbt_load_direction","Incoming");
+		}
 	}
 });
--- andesit_karang_anyar/andesit_karang_anyar/doctype/weighbridge_ticket/weighbridge_ticket.py
+++ andesit_karang_anyar/andesit_karang_anyar/doctype/weighbridge_ticket/weighbridge_ticket.py
@@ -5,6 +5,26 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class WeighbridgeTicket(Document):
-	pass
+	def validate(self):
+		self.validate_outgoing()
+		self.validate_incoming()
+
+	def validate_outgoing(self):
+		if self.wbt_load_direction=="Outgoing":
+			tolerance = (1.0*self.wbt_vehicle_tare_weight/100)*10
+			if self.wbt_first_weighing <= (tolerance+self.wbt_vehicle_tare_weight) and self.wbt_first_weighing >= (self.wbt_vehicle_tare_weight-tolerance):
+				net_weight = self.wbt_first_weighing - self.wbt_second_weighing
+			else:
+				frappe.throw(_("Tare Weight MisMatch"))
+				
+	def validate_incoming(self):
+		if self.wbt_load_direction=="Incoming":
+			tolerance =  (1.0*self.wbt_vehicle_tare_weight/100)*10
+			if self.wbt_second_weighing <= (tolerance+self.wbt_vehicle_tare_weight) and self.wbt_second_weighing >= (self.wbt_vehicle_tare_weight-tolerance):
+				net_weight = self.wbt_first_weighing - self.wbt_second_weighing
+			else:
+				frappe.throw(_("Tare Weight MisMatch"))
+			
