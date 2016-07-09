763697c6edb96586e7cef952e7985ff220daaaec
Vishal <vishaldhayagude07@gmail.com>
2016-07-09 14:24:05 +0530

    WBT SCRIPT Report with total. Issue #27

--- andesit_karang_anyar/andesit_karang_anyar/report/weighbridge_ticket/weighbridge_ticket.py
+++ andesit_karang_anyar/andesit_karang_anyar/report/weighbridge_ticket/weighbridge_ticket.py
@@ -13,21 +13,23 @@ def execute(filters=None):
 	entries = get_entries(filters)
 	data = []
 	total_wbt_net_weight = 0.0
+
+	# To find Total of net weight at the end of report.
 	for d in entries:
-		# total_wbt_net_weight += (d.wbt_net_weight)
+		total_wbt_net_weight += (d.wbt_net_weight)
 
 		data.append([
 			d.name, d.name, d.wbt_vehicle, d.workflow_state, d.wbt_net_weight
 		])
 
-	# if data:
-	# 	total_row = [""]*len(data[0])
-	# 	total_row[0] = _("Total")
-	# 	total_row[-1] = total_wbt_net_weight
-	# 	data.append(total_row)
+	if data:
+		total_row = [""]*len(data[0])
+		total_row[0] = _("Total")
+		total_row[-1] = total_wbt_net_weight
+		data.append(total_row)
 
 	return columns, data
-
+#Display Column row from Wieghbridge Ticket Report.
 def get_columns(filters):
 	if not filters.get("doc_type"):
 		msgprint(_("Please select the document type first"), raise_exception=1)
@@ -38,21 +40,22 @@ def get_columns(filters):
 		_("State") + ":Link/DocType:140",
 		_("Net Weight") + ":Float:140"]
 
+#Display Data in  Wieghbridge Ticket Report.
 def get_entries(filters):
 	# doc_field = filters["doc_type"] == 'Customer'
 	# conditions, values = get_conditions(filters, doc_field)
 	entries = frappe.db.sql("""
 		select
-			dt.name, wt.name, wt.wbt_vehicle, wt.workflow_state, wt.wbt_net_weight
-		from
-		    `tab%s` dt, `tabWeighbridge Ticket` wt
+			cs.name, wt.name, wt.wbt_vehicle, wt.workflow_state, wt.wbt_net_weight 
+		from 
+			`tab%s` cs, `tabWeighbridge Ticket` wt 
 		where  
-				wt.workflow_state = 'Weighing Complete'
-				and dt.docstatus = 1
-		""" %(filters["doc_type"]), as_list=1)
+			wt.workflow_state = 'Weighing Complete';
+		""" %(filters["doc_type"]), as_dict=1)
 
 	return entries
 
+# Start Wrok Here-- after break
 # def get_conditions(filters, doc_field):
 # 	conditions = [""]
 #  	values = []
